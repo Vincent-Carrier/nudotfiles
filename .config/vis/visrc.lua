9adf546c4d121a8c24ca858b30d9a5c895ccbb00
Vincent Carrier <vincentcarrier@icloud.com>
2021-03-14 20:57:41 -0400

    _

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,27 +1,56 @@
-require('vis')
+require'vis'
 
 vis.events.subscribe(vis.events.INIT, function()
+	vis:command'set shell /bin/zsh'
+
   vis:command'map! insert <C-d> <Delete>'
   vis:command'map! insert <C-b> <Left>'
   vis:command'map! insert <C-f> <Right>'
+  vis:command'map! insert <C-n> <Down>'
+  vis:command'map! insert <C-p> <Up>'
   vis:command'map! insert <C-a> <Home>'
   vis:command'map! insert <C-e> <End>'
-  vis:command'map! normal <C-q> ZZ'
+
+  vis:command'map! insert ( <C-v>u0028)<Left>'
+  vis:command'map! insert <M-9> <C-v>u0028'
+  vis:command'map! insert [ <C-v>u005b]<Left>'
+  vis:command'map! insert <M-[> <C-v>u005b'
+  vis:command'map! insert { <C-v>u007b}<Left>'
+  vis:command'map! insert <M-{> <C-v>u007b'
+
+  vis:command'map! normal <C-q> :wq<Enter>'
   vis:command'map! normal <C-s> :w<Enter>'
   vis:command'map! normal Y yy'
   vis:command'map! normal H ^'
   vis:command'map! normal L $'
+  vis:command'map! normal s o<C-c>k'
+  vis:command'map! normal S O<C-c>j'
+  vis:command'map! normal m \"_d'
+  vis:command'map! visual m \"_d'
+  vis:command'map! normal M \"_D'
+  vis:command'map! normal mm \"_dd'
   vis:command'map! visual Y :>pbcopy<Enter>'
   vis:command'map! visual P :<pbpaste<Enter>'
+  vis:command'map! visual <C-e> :>sendk<Enter>'
+  vis:command'map! normal <C-e> vap:>sendk<Enter>'
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+  if (win.file.name:match'%g+.janet') then
+    vis:command'set syntax lisp'
+  end
+
 	vis:command'set number'
-	vis:command'set theme default-256'
 	vis:command'set autoindent'
 	vis:command'set expandtab'
 	vis:command'set tabwidth 2'
 	vis:command'set cursorline'
+	
+	if os.getenv'LUX_THEME' == 'dark' then
+  	vis:command'set theme default-256'
+  else
+  	vis:command'set theme light-16'
+  end
 end)
 
 require'plugins/vis-commentary'
--- .zshenv
+++ .zshenv
@@ -1,8 +1,5 @@
 export ZDOTDIR=$HOME/.config/zsh
 export EDITOR=vise
 export VISUAL=vise
-export LUX_ITERM_ALL_LIGHT=Tomorrow
-export LUX_ITERM_ALL_DARK='Tomorrow Night'
-export FZF_DEFAULT_OPTS='--bind=ctrl-j:accept,ctrl-k:kill-line'
-export FZF_CTRL_T_COMMAND='fd --type f'
-export FZF_CTRL_T_OPTS="--preview 'bat --style=numbers --color=always --line-range :500 {}'  --height=50%"
+export PATH="$PATH:$HOME/go/bin:$HOME/.cargo/bin"
+source "$HOME/.cargo/env"
