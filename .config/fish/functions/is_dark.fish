db6e914f2c8cfdd62e34225e06bb564b8333a938
Vincent Carrier <vincentcarrier@icloud.com>
2021-03-25 19:51:14 -0400

    Better dark mode + kakoune daemon

--- .config/fish/functions/is_dark.fish
+++ .config/fish/functions/is_dark.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/gh/ps_g7zt12bq_0cdh9z3bsvpc0000gn/T//fish.BIHM8d/is_dark.fish @ line 2
+# Defined in /var/folders/gh/ps_g7zt12bq_0cdh9z3bsvpc0000gn/T//fish.Y2xuwc/is_dark.fish @ line 2
 function is_dark
     set is_dark (osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode.get()")
     if [ $is_dark = 'true' ]
--- .config/fish/functions/lightbulb.fish
+++ .config/fish/functions/lightbulb.fish
@@ -1,12 +1,17 @@
-# Defined in /var/folders/gh/ps_g7zt12bq_0cdh9z3bsvpc0000gn/T//fish.tRaodn/lightbulb.fish @ line 2
+# Defined in /var/folders/gh/ps_g7zt12bq_0cdh9z3bsvpc0000gn/T//fish.Yb8oJA/lightbulb.fish @ line 2
 function lightbulb
     if is_dark
       iterm_switch_profile 'Afterglow'
       set -Ux BAT_THEME ansi-dark
       set -U DARK true
+      set kak_theme "dark-mode"
     else
       iterm_switch_profile 'Tomorrow'
       set -Ux BAT_THEME ansi-light
       set -e DARK
+      set kak_theme "light-mode"
+    end
+    for s in (kak -l)
+      echo $kak_theme | kak -p $s
     end
 end
